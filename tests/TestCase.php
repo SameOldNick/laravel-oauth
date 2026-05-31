d5e4ea72a0aa656e92a963061acedc6351157207
Nick Hamnett <nick@sameoldnick.com>
2026-05-31 16:50:14 -0600

    Add base TestCase for package tests
    Introduce a TestCase class for package tests extending Orchestra\Testbench. It enables RefreshDatabase, WithLaravelMigrations and WithWorkbench traits, enables package discoveries, sets up the testing environment (default DB to testing and loads the package oauth config), and loads the package routes from routes/oauth.php. This provides a common test bootstrap for package test cases.

---
+++ tests/TestCase.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace SameOldNick\OAuth\Tests;
+
+use Illuminate\Config\Repository;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Routing\Router;
+use Orchestra\Testbench\Concerns\WithLaravelMigrations;
+use Orchestra\Testbench\Concerns\WithWorkbench;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+class TestCase extends Orchestra
+{
+    use RefreshDatabase;
+    use WithLaravelMigrations;
+    use WithWorkbench;
+
+    /**
+     * Automatically enables package discoveries.
+     *
+     * @var bool
+     */
+    protected $enablesPackageDiscoveries = true;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+    }
+
+    public function getEnvironmentSetUp($app)
+    {
+        tap($app['config'], function (Repository $config) {
+            $config->set('database.default', 'testing');
+
+            $config->set('oauth', require __DIR__.'/../config/oauth.php');
+        });
+    }
+
+    /**
+     * Define routes setup.
+     *
+     * @param  Router  $router
+     * @return void
+     */
+    protected function defineRoutes($router)
+    {
+        require __DIR__.'/../routes/oauth.php';
+    }
+}
