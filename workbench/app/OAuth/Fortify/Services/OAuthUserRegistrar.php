9f35896e82d608c25cd2e53f1cae33791a4dabbb
Nick Hamnett <nick@sameoldnick.com>
2026-06-01 23:51:52 -0600

    Verify OAuth user emails and accept Client
    Update OAuth user registration to accept the OAuth Client and optionally mark provider-verified emails as verified. The register method signature now takes a Client and SocialUser. Imported MustVerifyEmail and Client. After creating the user, if oauth.email_verification_required is enabled and the user implements MustVerifyEmail, the code checks provider-specific verification guarantees (Google: checks email_verified, Facebook: trusted, GitHub/Twitter/default: not trusted) and marks the email as verified when appropriate. Includes a TODO about using GitHub's /user/emails endpoint for stronger verification.

--- workbench/app/OAuth/Fortify/Services/OAuthUserRegistrar.php
+++ workbench/app/OAuth/Fortify/Services/OAuthUserRegistrar.php
@@ -4,8 +4,10 @@ namespace Workbench\App\OAuth\Fortify\Services;
 
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
 use Laravel\Socialite\Contracts\User as SocialUser;
+use SameOldNick\OAuth\Clients\Client;
 use SameOldNick\OAuth\Contracts\Services\OAuthUserRegistrar as OAuthUserRegistrarContract;
 
 class OAuthUserRegistrar implements OAuthUserRegistrarContract
@@ -19,7 +21,7 @@ class OAuthUserRegistrar implements OAuthUserRegistrarContract
     /**
      * {@inheritDoc}
      */
-    public function register(SocialUser $socialUser): Authenticatable
+    public function register(Client $client, SocialUser $socialUser): Authenticatable
     {
         // Re-use Laravel\Fortify's user creation logic to ensure things likeevents are properly handled.
         // We skip validation since OAuth users won't be providing a password during registration. They can set one later if they want to enable password login.
@@ -37,6 +39,12 @@ class OAuthUserRegistrar implements OAuthUserRegistrarContract
             $newUser->refresh();
         }
 
+        if ($this->isEmailVerificationRequired() &&
+            $newUser instanceof MustVerifyEmail &&
+            $this->isEmailVerifiedByOAuthProvider($client, $socialUser)) {
+            $newUser->markEmailAsVerified();
+        }
+
         // Needs to fire so things like emails can be sent.
         event(new Registered($newUser));
 
@@ -45,5 +53,41 @@ class OAuthUserRegistrar implements OAuthUserRegistrarContract
         }
 
         return $newUser;
+    }
+
+    /**
+     * Determines if email verification is required for users registering via OAuth.
+     *
+     * This checks the application configuration to see if email verification is required for OAuth registrations. If this returns true, then after creating the user, we will check if the email provided by the OAuth provider can be considered verified (based on the provider's guarantees and fields) and mark it as verified if so.
+     *
+     * @return bool True if email verification is required for OAuth registrations, false otherwise.
+     */
+    protected function isEmailVerificationRequired(): bool
+    {
+        return config('oauth.email_verification_required', false);
+    }
+
+    /**
+     * Determines if the email provided by the OAuth provider can be considered verified based on the provider's data and behavior.
+     *
+     * Since different providers have different guarantees and fields regarding email verification, we check based on the provider:
+     * - For providers that guarantee verified emails (e.g. Facebook), we can trust that the email is verified.
+     * - For providers that provide a specific field indicating verification (e.g. Google), we check that field.
+     * - For providers that do not guarantee or indicate email verification (e.g. GitHub, Twitter), we assume the email is not verified to be safe.
+     *
+     * @param  Client  $client  The OAuth client being used for authentication.
+     * @param  SocialUser  $socialUser  The user information returned by the OAuth provider.
+     * @return bool True if the email can be considered verified, false otherwise.
+     */
+    protected function isEmailVerifiedByOAuthProvider(Client $client, SocialUser $socialUser): bool
+    {
+        return match ($client->clientName()) {
+            // TODO: Use GitHub /user/emails endpoint to check if email is verified, since the main /user endpoint does not guarantee that the email is verified. For now, we assume it's not verified to be safe.
+            'github' => false, // The GitHub /user API endpoint does not guarantee that the email is verified.
+            'google' => $socialUser['email_verified'] ?? false, // Google provides an 'email_verified' field we can check.
+            'facebook' => true, // Facebook only returns verified emails, so we can trust that it's verified.
+            'twitter' => false, // No guarantee that Twitter emails are verified, and they don't provide a way to check, so we assume it's not verified.
+            default => false, // For other providers, we can't be sure, so we assume it's not verified.
+        };
     }
 }
